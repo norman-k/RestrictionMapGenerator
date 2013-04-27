3455250dcf413db71d831bfbb04d08ef26e617a0
normank01 <normank01@gmail.com>
2013-04-27 19:06:50 -0400

    first commit

---
+++ Plasmid.py
@@ -0,0 +1,10 @@
+
+def Plasmid_Generator():
+ with open("Plasmid.html","w",) as web_page:
+     web_page.write("<!doctype html>\n<html>\n<head>\n")
+     web_page.write("<link rel='stylesheet' type='text/css' href='stylesheet.css' />")
+     web_page.write("</head>\n<body>\n<p>\n")
+     web_page.write("<div></div>")
+     web_page.write("</p>\n</body>\n</html>\n")
+     
+
