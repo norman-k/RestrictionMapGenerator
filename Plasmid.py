9a3240dd364ac2b156eeb3841aa88e054bab9295
normank01 <normank01@gmail.com>
2013-04-27 21:56:19 -0400

    first commit

--- Plasmid.py
+++ Plasmid.py
@@ -7,20 +7,25 @@ while j <= z:
     k = input("Enter the size in kb of each fragment, denote a ',' for each one: ")
     j += 1
     g = []
-    g.append(x)
+    g.append(k)
     h.append(g)
 c = []
-    for b in c:
-        for a in c:
-            if a + b == x:
-               c.append(a)
-               c.append(b)
+for b in k:
+  for a in k:
+      if a + b == x:
+        c.append(a)
+        c.append(b)
+        
+def First_Map():
+    return str(c[0:2])
+
 def Plasmid_Generator():
  with open("Plasmid.html","w",) as web_page:
      web_page.write("<!doctype html>\n<html>\n<head>\n")
      web_page.write("<link rel='stylesheet' type='text/css' href='stylesheet.css' />")
      web_page.write("</head>\n<body>\n<p>\n")
      web_page.write("<div></div>")
+     web_page.write(First_Map())
      web_page.write("</p>\n</body>\n</html>\n")
      
-
+Plasmid_Generator()
