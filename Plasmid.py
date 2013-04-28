d807e99e2cdf2616242b23d2dd90cdef7f6c5a99
normank01 <normank01@gmail.com>
2013-04-27 20:36:49 -0400

    first commit

--- Plasmid.py
+++ Plasmid.py
@@ -1,4 +1,12 @@
-
+x = input("Please enter the length of the vector in kb for the initial restriction enzyme: ")
+y = input("Please enter the length of the insert in kb[Enter 0 if there is none]: ")
+z = input("How many more enzymes are there?: ")
+h = []
+j = 1
+while j <= z:
+    k = input("Enter the size in kb of each fragment, within a list: ")
+    j += 1
+    h.append(x) 
 def Plasmid_Generator():
  with open("Plasmid.html","w",) as web_page:
      web_page.write("<!doctype html>\n<html>\n<head>\n")
