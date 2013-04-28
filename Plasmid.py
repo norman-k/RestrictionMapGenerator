b89f48ee4cfdeb7c04ceebb41289f5afadac4632
normank01 <normank01@gmail.com>
2013-04-27 22:44:40 -0400

    first commit

--- Plasmid.py
+++ Plasmid.py
@@ -25,14 +25,14 @@ c.append(k[2:])
 try:
   for r in h[1]:
     for e in h[1]:
-        if r + e == x:
+        if r + e == max(g[0]):
             c.append(r)
             c.append(e)
 except:
   print "no other enzyme used"
 def First_Map_Slice():
-    f = x - max(k)
-    s = max(k)
+    f = x - max(g[0])
+    s = max(g[0])
     m = []
     m.append(f)
     m.append(s)
@@ -40,6 +40,8 @@ def First_Map_Slice():
 def First_Map_Finish():
     if y == 0:
        return str(k[1:])
+def Second_Map_Slice():
+    return c
 def Plasmid_Generator():
  with open("Plasmid.html","w",) as web_page:
      web_page.write("<!doctype html>\n<html>\n<head>\n")
@@ -50,6 +52,8 @@ def Plasmid_Generator():
      web_page.write(First_Map_Slice())
      web_page.write("Then on one side, slice it into: \n")
      web_page.write(First_Map_Finish())
+     web_page.write("Then on the other: \n")
+     web_page.write(Second_Map_Slice())
      web_page.write("</p>\n</body>\n</html>\n")
      
 Plasmid_Generator()
