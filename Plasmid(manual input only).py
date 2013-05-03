879506cbb8b8f9c6e14ae7bf4671a7a31bbd37a3
normank01 <normank01@gmail.com>
2013-05-03 19:25:04 -0400

    fix integer extraction

--- Plasmid(manual input only).py
+++ Plasmid(manual input only).py
@@ -49,7 +49,7 @@ def Second_Map_Slice():
 def Plasmid_Generator():
  with open("Plasmid.html","w",) as web_page:
      web_page.write("<!doctype html>\n<html>\n<head>\n")
-     web_page.write("<link rel='stylesheet' type='text/css' href='stylesheet.css' />")
+     web_page.write("<link rel='stylesheet' type='text/css' href='stylesheet.css' />\n")
      web_page.write("</head>\n<body>\n<p>\n")
      web_page.write("<div></div>")
      web_page.write("First, slice it into\n")
